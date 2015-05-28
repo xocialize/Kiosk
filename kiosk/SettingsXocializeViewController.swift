dab6d589bd73eee1364418f01d4e012407c91073
xocialize <dustin.nielson@gmail.com>
2015-05-27 17:40:58 -0700

    more xocialize integration stuff

--- kiosk/SettingsXocializeViewController.swift
+++ kiosk/SettingsXocializeViewController.swift
@@ -9,11 +9,74 @@
 import UIKit
 
 class SettingsXocializeViewController: UIViewController {
-
+    
+    var dm: DataManager = DataManager()
+    
+    var settings:Dictionary<String,AnyObject> = [:]
+    
+    @IBOutlet var infoLabel: UILabel!
+    
+    @IBOutlet var enableSwitch: UISwitch!
+    
+    @IBAction func cancelButton(sender: AnyObject) {
+        
+        performSegueWithIdentifier("xocializeToSettingsSegue", sender: self)
+        
+    }
+    
+    @IBAction func updateButton(sender: AnyObject) {
+        
+        if enableSwitch.on {
+            
+            settings["xocializeEnabled"] = true
+            
+            
+        } else {
+            
+            settings["xocializeEnabled"] = false
+            
+        }
+        
+        dm.saveSettings(settings)
+        
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        settings = dm.getSettings()
+        
+        if let enabled = settings["xocializeEnabled"] as? Bool {
+            
+            enableSwitch.setOn(enabled, animated:true)
+            
+        } else {
+            
+            enableSwitch.setOn(false, animated:true)
+            
+        }
+        
+        enableSwitch.addTarget(self, action: Selector("stateChanged:"), forControlEvents: UIControlEvents.ValueChanged)
+        
+    }
+    
+    func stateChanged(switchState: UISwitch) {
+        
+        if enableSwitch.on {
+        
+            println("The Switch is On")
+        
+        } else {
+        
+            println("The Switch is Off")
+        
+        }
+    }
+    
+    override func viewDidLayoutSubviews() {
+        
+        infoLabel.text = "Xocialize provides remote management for your Kiosk. \n\nIn order to add this device to Xocialize you will need a barcode generated at https://xocialize.com under the Kiosk > Devices section of the admin interface."
+    
     }
 
     override func didReceiveMemoryWarning() {
